0f2396bb4fe89ed5bcf9fbd2c033bd26b390cd87
amalasyrafs <57763629+amalasyrafs@users.noreply.github.com>
2021-11-10 17:22:03 +0800

    details UI

--- src/containers/dashboard/details.js
+++ src/containers/dashboard/details.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useLocation } from 'react-router';
 import classes from './details.module.css';
+import Product from '../../components/product';
 
 const Details = () => {
   let location = useLocation();
   let product = location.state;
-  
+
   return (
-    <div>
-      <div className={classes.product_img}></div>
+    <div className={classes.detailsContainer}>
+      <Product
+        product={product}
+        prodID={product.id}
+        src={product.image}
+        name={product.name}
+        price={product.cost}
+        shortDesc={product.short_description}
+      />
+      {/* <div className={classes.product_img}></div>
       <div>
         <h1>{product.name}</h1>
         <h4>{product.short_description}</h4>
@@ -19,7 +28,7 @@ const Details = () => {
 
         <h5>Price: {product.cost}</h5>
         <button>Buy now</button>
-      </div>
+      </div> */}
     </div>
   );
 };
